3f600afc996ca09ab87e0a97d2d5bf336efee141
Naman Sharma <sharmanammu1208@gmail.com>
2023-04-04 00:06:13 +0530

    dooone

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="pl-9 rounded-2xl mx-auto w-2/3 md:w-full"
           />
         </div>
       </div>
