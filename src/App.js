32f4cbde16825831137b0f0b331862622eef8c4a
Naman Sharma <sharmanammu1208@gmail.com>
2023-04-03 23:46:34 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SocialLinks from "./components/SocialLinks";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contacts from "./components/Contacts";
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div> 
+      <NavBar /> 
+      <Home />
+      <About />
+      <Portfolio />
+      <SocialLinks />
+      <Experience/>
+      <Contacts/>
     </div>
+
   );
 }
 
